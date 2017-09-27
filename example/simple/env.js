6c265dacc1d685a7e567f7a3c87e331a3cd12061
ufologist <klsforever@gmail.com>
2017-09-27 16:40:03 +0800

    添加模板使用的说明

--- example/simple/env.js
+++ example/simple/env.js
@@ -2,18 +2,23 @@
 // 详见: https://github.com/ufologist/wieldy-webpack/blob/master/src/config/env.js
 var env = {
     dev: {
-        __api_root_endpoint__: ''
+        __api_root_endpoint__: '',
+        __page_data__: { // 一般用作后端给页面中灌入的首屏数据
+            foo: 1,
+            bar: 'bar'
+        }
     },
     prod: {
+        __api_root_endpoint__: '//api.domain.com',
+        __page_data__: '!{pageData}', // 正式环境时直接使用模板页面中的变量, 例如 velocity 变量
+
         __public_base_path__: '//cdn.com/',
 
         __ftp_host__: 'ftp.cdn.com',
         __ftp_port__: 2121,
         __ftp_user__: 'test',
         __ftp_password__: 'test',
-        __ftp_base_path__: '/project/',
-
-        __api_root_endpoint__: '//api.domain.com'
+        __ftp_base_path__: '/project/'
     }
 };
 
