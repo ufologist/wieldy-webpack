7d85059c48c454eced1b91171dd4089bee453f31
ufologist <klsforever@gmail.com>
2018-01-16 15:32:52 +0800

    修改注释

--- example/simple/env.js
+++ example/simple/env.js
@@ -1,5 +1,8 @@
 // 覆盖 wieldy-webpack 默认的环境相关配置
 // 详见: https://github.com/ufologist/wieldy-webpack/blob/master/src/config/env.js
+
+var metaViewport = '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+
 var vConsole = `
 <script src="//wechatfe.github.io/vconsole/lib/vconsole.min.js?v=3.0.0.0"></script>
 <script>window.vConsole = new VConsole();</script>
@@ -8,8 +11,9 @@ var vConsole = `
 var env = {
     dev: {
         __api_root_endpoint__: '',
-        // 用于在页面头部中统一注入资源, 例如统计和调试用的脚本
-        __common_head__: vConsole,
+        // 用于在页面 <head> 中统一注入资源, 例如统计和调试用的脚本
+        __common_head__: metaViewport + vConsole,
+        // 用于在页面 <body> 中统一注入资源, 例如初始化的 Loading 提示
         __common_body__: '',
         __page_data__: { // 一般用作后端给页面中灌入的首屏数据
             foo: 1,
@@ -18,7 +22,7 @@ var env = {
     },
     prod: {
         __api_root_endpoint__: '//api.domain.com',
-        __common_head__: '',
+        __common_head__: metaViewport,
         __common_body__: '',
         __page_data__: '!{pageData}', // 正式环境时直接使用模板页面中的变量, 例如 velocity 变量
 
